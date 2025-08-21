9b90b7c36878b9a5938bc4c31ac204e70fd10483
viswajith k k <viswajithkanayi@gmail.com>
2025-08-21 13:35:03 +0530

    mongodb configuration changed

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-mongoose.connect(process.env.DB_URL||"mongodb://localhost:27017/FashionClub")
+mongoose.connect(process.env.DB_URL)
 
 mongoose.connection.on("connected", () => {
     console.log("connected to mongodb");
